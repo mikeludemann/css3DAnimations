9c6aeb037eef9a5c4fbf586f4939bbe366f26c22
Mike Ludemann <mike-ludemann@hotmail.de>
2018-09-07 17:42:41 +0200

    First steps with examples

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSS 3D Amimations
+
+Simple tutorial and helper for some CSS3 3D animations
---
+++ src/elementFlipHorizontal/script/script.js
@@ -0,0 +1,7 @@
+var element = document.querySelector('.container__sub');
+
+element.addEventListener('click', function () {
+
+    element.classList.toggle('flip');
+
+});
