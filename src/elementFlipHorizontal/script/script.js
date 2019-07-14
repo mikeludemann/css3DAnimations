4e0acd22b1e587f1336996dfb99c00872e6f9c31
Mike Ludemann <mike-ludemann@hotmail.de>
2019-07-14 15:06:31 +0200

    Bugfixing at elementFlipHorizontal and elementFlipVertical

--- src/elementFlipHorizontal/script/script.js
+++ src/elementFlipHorizontal/script/script.js
@@ -1,7 +1,23 @@
 var element = document.querySelector('.container__sub');
 
+element.addEventListener('mouseover', function () {
+
+	element.classList.add('flip');
+
+});
+
+element.addEventListener('mouseout', function () {
+
+	element.classList.remove('flip');
+
+});
+
+/* ################################## */
+
+var element = document.querySelector('.container__sub');
+
 element.addEventListener('click', function () {
 
-    element.classList.toggle('flip');
+	element.classList.toggle('flip');
 
 });
